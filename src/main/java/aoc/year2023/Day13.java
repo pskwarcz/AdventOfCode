2bf6f8c9c87c1c27f388568634f9d06eab6ee9e4
pskwarcz <p.skwarcz@gmail.com>
2023-12-13 07:44:36 +0100

    Day13 part1

---
+++ src/main/java/aoc/year2023/Day13.java
@@ -0,0 +1,111 @@
+package aoc.year2023;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+import aoc.Utils;
+
+public class Day13 {
+
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		new Day13().start();
+		long end = System.currentTimeMillis();
+		long duration = end - start;
+		System.out.println("\nTime[ms]: " + duration);
+	}
+
+	private void start() {
+		List<String> lines = Utils.readFile("/aoc/year2023/input13.txt");
+
+		long result = process(lines);
+
+		System.out.println("\nresult: " + result);
+	}
+
+	long process(List<String> lines) {
+		Iterator<String> it = lines.iterator();
+		long sum = 0;
+		while (it.hasNext()) {
+			List<String> pattern = readPattern(it);
+			sum += reflectionNumber(pattern);
+			System.out.println("");
+		}
+
+		return sum;
+	}
+
+	private long reflectionNumber(List<String> pattern) {
+		for (String s : pattern) {
+			System.out.println(s);
+		}
+		long n = 0;
+		n += verticalReflection(pattern);
+		n += 100 * horizontalReflection(pattern);
+		return n;
+	}
+
+	private long verticalReflection(List<String> pattern) {
+		for (int x = 1; x < pattern.get(0).length(); x++) {
+			if (isVerticalReflection(pattern, x)) {
+				System.out.println("v=" + x);
+				return x;
+			}
+		}
+		return 0;
+	}
+
+	private boolean isVerticalReflection(List<String> pattern, int x) {
+		for (String line : pattern) {
+			if (!isVerticalReflection(line, x)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private boolean isVerticalReflection(String line, int x) {
+		for (int i = 1; x - i >= 0 && x - 1 + i < line.length(); i++) {
+			if (line.charAt(x - i) != line.charAt(x - 1 + i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private int horizontalReflection(List<String> pattern) {
+		for (int y = 1; y < pattern.size(); y++) {
+			if (isHorizontalReflection(pattern, y)) {
+				System.out.println("h=" + y);
+				return y;
+			}
+		}
+		return 0;
+	}
+
+	// y=4
+	private boolean isHorizontalReflection(List<String> pattern, int y) {
+		for (int i = 1; y - i >= 0 && y - 1 + i < pattern.size(); i++) {
+			if (!pattern.get(y - i).equals(pattern.get(y - 1 + i))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private List<String> readPattern(Iterator<String> it) {
+		List<String> pattern = new ArrayList<>();
+		while (it.hasNext()) {
+			String line = it.next();
+			if (StringUtils.isEmpty(line)) {
+				break;
+			}
+			pattern.add(line);
+		}
+		return pattern;
+	}
+
+}
