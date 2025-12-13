65449a4bd2e8a01d361f18460586fefa775be015
pskwarcz <p.skwarcz@gmail.com>
2025-12-13 23:27:49 +0100

    Dy25Test

--- src/test/java/aoc/year2023/Day25Test.java
+++ src/test/java/aoc/year2023/Day25Test.java
@@ -3,6 +3,10 @@ package aoc.year2023;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
@@ -32,6 +36,24 @@ public class Day25Test {
 		List<String> lines = Utils.readFile("/aoc/year2023/input25.txt");
 		long res = new Day25().process(lines);
 		assertThat(res, is(592171L));
+	}
+
+	@Test
+	public void pr() throws InterruptedException {
+		Collection<Integer> syncCollection = Collections.synchronizedCollection(new ArrayList<>());
+		Runnable listOperations = () -> {
+			syncCollection.addAll(Arrays.asList(1, 2, 3, 4, 5, 6));
+		};
+
+		Thread thread1 = new Thread(listOperations);
+		Thread thread2 = new Thread(listOperations);
+		thread1.start();
+		thread2.start();
+		thread1.join();
+		thread2.join();
+
+		System.out.println(syncCollection);
+
 	}
 
 }
