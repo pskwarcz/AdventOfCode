a22ae03a98ad631a0bcb76f04b95c79d1fd4f6a2
Pawel Skwarcz <pskwarcz@icm.edu.pl>
2025-12-15 08:56:56 +0100

    day05 Cafeteria part two first try

---
+++ src/main/java/aoc/year2025/Day05b.java
@@ -0,0 +1,91 @@
+package aoc.year2025;
+
+import aoc.Utils;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Day05b {
+
+    static void main() {
+        long start = System.currentTimeMillis();
+        new Day05b().start();
+        long end = System.currentTimeMillis();
+        long duration = end - start;
+        System.out.println("\nTime[ms]: " + duration);
+    }
+
+    void start() {
+        List<String> lines = Utils.readFile("/aoc/year2025/input05");
+        long result = process(lines);
+        System.out.println("\nresult: " + result);
+    }
+
+    long process(List<String> lines) {
+        Iterator<String> it = lines.iterator();
+        List<Range> ranges = new ArrayList<>();
+
+        String line = it.next();
+        while (!line.isBlank()) {
+            ranges.add(new Range(line));
+            line = it.next();
+        }
+
+        List<Range> merged = new LinkedList<>();
+        for (Range r : ranges) {
+
+            Iterator<Range> mit = merged.iterator();
+            while (mit.hasNext()) {
+                Range m = mit.next();
+                if (r.overlaps(m)) {
+                    r = m.mergeWith(r);
+
+                    mit.remove();
+                }
+            }
+            merged.add(r);
+            IO.println(merged);
+        }
+
+        return merged.stream().mapToLong(Range::size).sum();
+    }
+
+    record Range(long start, long end) {
+        public Range {
+            if (start > end) {
+                throw new IllegalArgumentException("Invalid range:" + start + ", " + end);
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "(" + start + "-" + end + ')';
+        }
+
+        public Range(String line) {
+            String[] l = line.split("-");
+            this(Long.parseLong(l[0]), Long.parseLong(l[1]));
+        }
+
+        boolean contains(long value) {
+            return value >= start && value <= end;
+        }
+
+        public long size() {
+            return end - start + 1;
+        }
+
+        public Range mergeWith(Range r) {
+            Range merged = new Range(Math.min(r.start, this.start), Math.max(r.end, this.end));
+            IO.println("\t Merging: " + this.toString() + " + " + r + " = " + merged);
+            return merged;
+        }
+
+        public boolean overlaps(Range m) {
+            return contains(m.start()) || contains(m.end());
+        }
+    }
+
+}
--- src/test/java/aoc/year2025/Day05Test.java
+++ src/test/java/aoc/year2025/Day05Test.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -31,5 +32,22 @@ class Day05Test {
         assertThat(res, is(701L));
     }
 
+    @Test
+    void testDataPart2() {
+        List<String> lines = Utils.readFile(TEST_INPUT);
+
+        long res = new Day05b().process(lines);
+
+        assertThat(res, is(14L));
+    }
+
+    @Test
+    void testRealDataPart2() {
+        List<String> lines = Utils.readFile(INPUT);
+
+        long res = new Day05b().process(lines);
+
+        assertThat(res, not(is(357537524174696L))); // too high
+    }
 }
 
